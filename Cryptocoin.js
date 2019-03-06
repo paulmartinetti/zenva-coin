13c6cd1deee721363a65c39c8f4d7788843c2c08
Paul Martinetti <paul@digscitech.com>
2019-03-06 08:16:41 -0500

    Created Cryptocoin class, parsed JSON into objs

---
+++ Cryptocoin.js
@@ -0,0 +1,13 @@
+// the exports. syntax is specifically for nodejs
+exports.Cryptocoin = class {
+    constructor(id, name, price_usd) {
+        this.id = id;
+        this.name = name;
+        this.price_usd = price_usd;
+    }
+
+    // method to convert
+    convert_to (other_coin) {
+        return this.price_usd / other_coin.price_usd;
+    }
+}
--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // This example is in Node ES6 using the request-promise library
 const rp = require('request-promise');
 const http = require('http');
+// we can put require here only because we wrote 'export.' in the class
+const cryptocoin = require('./Cryptocoin.js');
+let coinsA = [];
 
 /* start nodejs
 * 1. npm init
@@ -13,8 +16,8 @@ const http = require('http');
 *
 *
 */
-
-let coin_data = undefined;
+// this var was for testing the remoting call
+//let coin_data = undefined;
 
 const requestOptions = {
     method: 'GET',
@@ -33,18 +36,24 @@ const requestOptions = {
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    if (coin_data) {
-        res.end(JSON.stringify(coin_data));
+    if (coinsA) {
+        res.end(JSON.stringify(coinsA));
     } else {
         res.end('No data');
     }
 }).listen(8080);
 
-// format given on coin site
+// format for nodejs API given on coin site
 rp(requestOptions).then(body => {
-    //console.log('API call response:', response);
-    coin_data = body.data;
-    console.log(coin_data);
+    let coin_data = body.data;
+    //console.log(coin_data[1]); <-- ran this to see where price is stored
+    // then tested here vv
+    //console.log(coin_data[1].quote.USD.price);
+    coin_data.forEach(coin => {
+        coinsA.push(new cryptocoin.Cryptocoin(coin.id, coin.name, coin.quote.USD.price));
+    });
+    //coin_data = body.data;
+    //console.log(coin_data);
 }).catch((err) => {
     console.log('API call error:', err.message);
 });
